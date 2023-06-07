bba3558bdb31efcf4c139f9f0422144a191f0d8e
Arefin Aareef <arefin.aareef@gmail.com>
2023-06-08 01:02:33 +0600

    auth provider, react-hook-form

--- client/src/Routes/Routes.jsx
+++ client/src/Routes/Routes.jsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Login/Login";
 import Menu from "../pages/Menu/Menu/Menu";
 import Order from "../pages/Order/Order/Order";
+import SignUp from "../pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([
   {
@@ -20,6 +22,14 @@ export const router = createBrowserRouter([
         {
           path: 'order/:category',
           element: <Order></Order>
+        },
+        {
+          path: 'login',
+          element: <Login></Login>
+        },
+        {
+          path: 'signup',
+          element: <SignUp></SignUp>
         }
     ]
   },
---
+++ client/src/pages/Login/Login.jsx
@@ -0,0 +1,98 @@
+import { useContext, useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+import { AuthContext } from '../../providers/AuthProvider';
+
+const Login = () => {
+    const captchaRef = useRef(null)
+    const [disabled, setDisabled] = useState(true)
+    const {signIn} = useContext(AuthContext)
+
+    useEffect(() => {
+        loadCaptchaEnginge(6); 
+    }, [])
+
+    const handleValidateCaptcha = () => {
+        const user_captcha_value = captchaRef.current.value;
+        if (validateCaptcha(user_captcha_value)) {
+            setDisabled(false)
+        }
+        else {
+            setDisabled(true)
+        }
+    }
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const formData = { email, password }
+        console.log(formData);
+        signIn(email, password)
+        .then(result => {
+          const user = result.user;
+          console.log(user);
+        })
+    }
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col md:flex-row-reverse">
+        <div className="text-center md:w-1/2 lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+        </div>
+        <div className="card md:w-1/2 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handleLogin} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control">
+              <label className="label">
+              <LoadCanvasTemplate />
+              </label>
+              <input
+                type="text"
+                name="captcha"
+                ref={captchaRef}
+                placeholder="enter the text above"
+                className="input input-bordered"
+              />
+              <button onClick={handleValidateCaptcha} className="btn btn-outline btn-xs my-2">Validate</button>
+            </div>
+            <div className="form-control mt-6">
+              <input disabled={disabled} className="btn btn-primary" type="submit" value="Login" />
+            </div>
+          </form>
+          <p><small>New Here? <Link to='/signup'>Create an account</Link></small></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
