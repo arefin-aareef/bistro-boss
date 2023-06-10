c9c106925ffc3ec744dfa68ec654ebdbd7990d0f
Arefin Aareef <arefin.aareef@gmail.com>
2023-06-10 13:15:28 +0600

    amin link secure with admin route

---
+++ client/src/pages/Dashboard/AddItem/AddItem.jsx
@@ -0,0 +1,11 @@
+
+
+const AddItem = () => {
+    return (
+        <div>
+            <h2 className="text-3xl text-center font-semibold">Add New Item</h2>
+        </div>
+    );
+};
+
+export default AddItem;
